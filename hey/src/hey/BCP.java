9648fd22371a052dd9fb038b05c3544ecf243d52
C5265038 <C5265038@AGRN50936803A.apj.global.corp.sap>
2018-10-22 13:08:14 +0530

    my second cummit ji

---
+++ hey/src/hey/BCP.java
@@ -0,0 +1,13 @@
+package hey;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BCP {
+	public static void main(String[] args) {
+		WebDriver driver=new FirefoxDriver();
+		driver.get("https://support.wdf.sap.corp/sap(bD1lbiZjPTAwMSZkPW1pbiZpPTE=)/bc/bsp/sap/crm_ui_start/default.htm?saprole=ZCSSINTPROCE&crm-object-type=CRM_SRQM_INCIDENT&crm-object-keyname=OBJECT_GUID&crm-object-value=0090FAE68ED01ED8A6C1C1549BEC80D5");
+		
+	}
+
+}
